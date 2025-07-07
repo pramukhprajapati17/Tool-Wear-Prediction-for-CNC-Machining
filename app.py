33c8c0bf221dc58f594ba2614700e435964a8ea6
pramukhprajapati17 <pramukhprajapati17@gmail.com>
2025-07-07 14:24:10 +0530

    updated app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import joblib
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 import seaborn as sns
 import os
 from datetime import datetime
